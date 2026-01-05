9e22f834d0a94e82c8b858386764007bea2d290f
Midhun Unni <96683120+midhununni457@users.noreply.github.com>
2026-01-05 19:32:11 +0530

    firebase init

---
+++ app/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGxLSrBHA6czAfaycWFuv_6-6dJKlsqIE",
+  authDomain: "fir-workshop-450d7.firebaseapp.com",
+  projectId: "fir-workshop-450d7",
+  storageBucket: "fir-workshop-450d7.firebasestorage.app",
+  messagingSenderId: "850785430846",
+  appId: "1:850785430846:web:4daa93cb5ca2f8804b506f",
+  measurementId: "G-Q1D26RWXZ9"
+};
+
+const app = initializeApp(firebaseConfig);
