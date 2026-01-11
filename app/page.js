c2aababb843025582c95aa0ae639c2692117d8c7
Midhun Unni <96683120+midhununni457@users.noreply.github.com>
2026-01-12 01:43:22 +0530

    add update & delete post features

--- app/page.js
+++ app/page.js
@@ -1,16 +1,29 @@
 "use client";
 import { useState, useEffect } from "react";
-import { db } from "./config/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+import { db, auth } from "./config/firebase";
+import { signOut } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
   const postsCollectionRef = collection(db, "posts");
+  const router = useRouter();
+  const user = auth.currentUser;
+  const email = user ? user.email : null;
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const data = await getDocs(postsCollectionRef);
+        const q = query(postsCollectionRef, orderBy("created_at", "desc"));
+        const data = await getDocs(q);
         const filteredData = data.docs.map((doc) => ({
           ...doc.data(),
           id: doc.id,
@@ -23,15 +36,44 @@ export default function Home() {
     fetchPosts();
   }, []);
 
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      router.push("/login");
+    } catch (err) {
+      console.error("Error during logout:", err);
+    }
+  };
+
+  const handleDelete = async (postId) => {
+    try {
+      await deleteDoc(doc(db, "posts", postId));
+      setPosts(posts.filter((post) => post.id !== postId));
+    } catch (err) {
+      console.error("Error deleting post:", err);
+    }
+  };
+
   return (
     <div className="min-h-screen">
       {/* Navbar */}
       <nav className="bg-[#1F1F1F] border-b border-zinc-600 sticky top-0 z-50">
         <div className="max-w-4xl mx-auto px-6 py-4 flex items-center justify-between">
-          <h1 className="text-2xl text-gray-200 font-bold">MECInsta</h1>
-          <button className="text-gray-200 hover:text-[#FFCA28] hover:cursor-pointer font-semibold transition-colors duration-200">
-            Logout
-          </button>
+          <h1 className="text-3xl text-[#BF360C] font-bold">Readdit</h1>
+          <div className="flex gap-10 items-end">
+            <button
+              onClick={() => router.push("/create")}
+              className="text-gray-200 hover:text-[#FFCA28] hover:cursor-pointer font-semibold transition-colors duration-200"
+            >
+              Create
+            </button>
+            <button
+              onClick={handleLogout}
+              className="text-gray-200 hover:text-[#FFCA28] hover:cursor-pointer font-semibold transition-colors duration-200"
+            >
+              Logout
+            </button>
+          </div>
         </div>
       </nav>
 
@@ -49,27 +91,39 @@ export default function Home() {
                 key={post.id}
                 className="rounded-lg bg-[#1F1F1F] border border-zinc-600 overflow-hidden shadow-lg"
               >
-                {/* User */}
-                <div className="px-4 py-4 flex items-center gap-3">
-                  <p className="text-gray-200 font-semibold">
-                    {post.user || "Anonymous"}
-                  </p>
-                </div>
-
-                {/* Image */}
-                {post.img_url && (
-                  <div className="bg-[#121212] max-h-125 overflow-hidden">
-                    <img
-                      src={post.img_url}
-                      alt="Post"
-                      className="w-full h-auto object-cover"
-                    />
-                  </div>
-                )}
-
-                {/* Caption */}
+                {/* Content */}
                 <div className="px-6 py-4">
-                  <p className="text-gray-200">{post.caption}</p>
+                  <div className="flex items-center justify-between">
+                    <h2 className="text-2xl font-semibold text-gray-200">
+                      {post.title || "Untitled"}
+                    </h2>
+                    {email === post.user && (
+                      <div className="space-x-3">
+                        <button
+                          onClick={() => {
+                            router.push(`/update/${post.id}`);
+                          }}
+                          className="bg-[#FFCA28] hover:cursor-pointer rounded-md px-3 py-1"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          onClick={() => {
+                            handleDelete(post.id);
+                          }}
+                          className="bg-[#BF360C] hover:cursor-pointer rounded-md px-3 py-1"
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                  <span className="mt-1 block text-sm text-zinc-500">
+                    by {post.user || "Anonymous"}
+                  </span>
+                  <p className="mt-4 text-gray-300 whitespace-pre-line">
+                    {post.description || ""}
+                  </p>
                 </div>
               </div>
             ))}
--- app/update/[postId]/page.js
+++ app/update/[postId]/page.js
@@ -1,43 +1,53 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState, use } from "react";
 import { useRouter } from "next/navigation";
-import { db, auth } from "../config/firebase";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { getDoc, updateDoc, doc } from "firebase/firestore";
 
-export default function CreatePost() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const postsCollectionRef = collection(db, "posts");
+export default function UpdatePost({ params }) {
+  const { postId } = use(params);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
   const router = useRouter();
-  const user = auth.currentUser;
-  const email = user ? user.email : "Anonymous";
 
-  const onCreatePost = async () => {
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const postSnap = await getDoc(doc(db, "posts", postId));
+        const postData = postSnap.data();
+        setNewTitle(postData.title || "");
+        setNewDescription(postData.description || "");
+      } catch (err) {
+        console.error("Error fetching post:", err);
+      }
+    };
+    fetchPost();
+  }, []);
+
+  const onUpdatePost = async () => {
     try {
-      await addDoc(postsCollectionRef, {
-        title,
-        description,
-        created_at: serverTimestamp(),
-        user: email,
+      await updateDoc(doc(db, "posts", postId), {
+        title: newTitle,
+        description: newDescription,
       });
       router.push("/");
     } catch (err) {
-      console.error("Error while creating post:", err);
+      console.error("Error while updating post:", err);
     }
   };
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="w-full max-w-xl p-8 flex flex-col rounded-lg bg-[#1F1F1F] border border-zinc-600 shadow-lg">
-        <h1 className="text-4xl text-gray-200 font-bold mb-8">Create Post</h1>
+        <h1 className="text-4xl text-gray-200 font-bold mb-8">Update Post</h1>
 
         <div className="flex flex-col space-y-6">
           <div className="flex flex-col space-y-2">
             <label className="text-sm text-zinc-400">Title</label>
             <input
               type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
               placeholder="Give your post a title"
               className="p-3 rounded-md bg-[#121212] text-white caret-white border focus:outline-none focus:ring-2 focus:ring-[#FFCA28] border-zinc-600 placeholder-zinc-600"
             />
@@ -46,8 +56,8 @@ export default function CreatePost() {
           <div className="flex flex-col space-y-2">
             <label className="text-sm text-zinc-400">Description</label>
             <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
               placeholder="Share the details..."
               className="min-h-30 resize-none p-3 rounded-md bg-[#121212] text-white caret-white border focus:outline-none focus:ring-2 focus:ring-[#FFCA28] border-zinc-600 placeholder-zinc-600"
             />
@@ -55,7 +65,7 @@ export default function CreatePost() {
 
           <button
             type="button"
-            onClick={onCreatePost}
+            onClick={onUpdatePost}
             className="w-full bg-[#E65100] hover:bg-[#BF360C] text-white font-semibold py-3 px-6 rounded-md transition-colors duration-200 cursor-pointer"
           >
             Post
